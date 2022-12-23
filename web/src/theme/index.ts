2fb94c75e96eb3ea82afb017c707aa996dbe252f
S1lentcodes <Silentcodes@protonmail.com>
2022-12-22 21:29:22 -0500

    Basic theme, dont got time to fix their stuff to fully support theme

--- web/src/theme/index.ts
+++ web/src/theme/index.ts
@@ -6,4 +6,17 @@ const config: ThemeConfig = {
 
 export const theme = extendTheme({
   config,
+  semanticTokens: {
+    colors: {
+      'chakra-body-text': 'blue', // Text color of context menu
+    },
+  },
+  colors: {
+    gray: {
+      600: 'orange', // Secondary background of context menu
+      700: 'green', // Hover color of context menu
+      800: 'red', // Main background of context menu
+    },
+    "chakra-body-text": "blue",
+  },
 });
